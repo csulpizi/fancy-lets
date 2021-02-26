b3550efdc694538b9b951a261ea1912a9c837512
Cory Sulpizi <cory.sulpizi@gmail.com>
2021-02-26 00:06:16 -0800

    Better docs

--- src/fancy_lets/core.clj
+++ src/fancy_lets/core.clj
@@ -8,9 +8,20 @@
                             " simple symbol or `:validate`. Instead received " a)))))
 
 (defmacro let-catch
- "
-   See `fancy-lets.examples` for examples."
+  "Recursive version of `let` that can be configured to short-circuit.
+   Adding the keyword `:validate` and an assoicated `:else` performs
+   runs the validate expression. If the expression returns
+   nil or false, the let statement is short-circuited and the
+   else expression executed and returned.
 
+   e.g. (let-catch [a 3
+                    :validate (odd? a) :else (str a \" is not odd.\")
+                    b 4
+                    :validate (odd? b) :else (str b \" is not odd.\")]
+         (+ a b))
+         Would return: \"4 is not odd.\"
+
+   See `fancy-lets.examples` for more examples."
   [bindings & body]
   (if (empty? bindings)
     `(do ~@body)
@@ -25,9 +36,18 @@
             :else (throw-let-catch-exception a b c d)))))
 
 (defmacro let-catch-all
-  "
-   See `fancy-lets.examples` for examples."
+  "Similar to `let-catch` except you provide a `pred` and `fail-f`. Each
+   bound value is checked after it is bound. If the (pred val) returns
+   nil or false, (fail-f val) is evaluated and the let statement short-circuits.
 
+   e.g. The following example is identical to the above example:
+        (let-catch-all odd? #(str % \" is not odd.\")
+           [a 3
+            b 4]
+         (+ a b))
+        Which would return: \"4 is not odd.\"
+
+   See `fancy-lets.examples` for more examples."
   [pred fail-f bindings & body]
   (if (empty? bindings)
     `(do ~@body)
